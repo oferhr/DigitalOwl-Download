3398abab995a479fbed898d41f7912850add0fca
Ofer Harari <offa23@hotmail.com>
2022-06-04 08:33:53 +0000

    version 1.0.2 add support to new lines in excel add archived status
    git-tfs-id: [https://oferh.visualstudio.com/DefaultCollection]$/Projects/DigitalOwl-Download;C200

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace DigitalOwl_Download
         private static string downloadDir;
         private static string CurrentBLine;
         private static string excelFile;
+        private static string ArchiveText = "ארכיון";
         private static string KEY = "eyJjbGllbnRfaWQiOiI0dzFPNUlJTE9GajdSajhvckZqTkJvR3Z4RVkwNlhUQyIsImNsaWVudF9zZWNyZXQiOiJsYWNBcFd0VTFHeXRfSVNlVGZCZVdweGRBRVJ3NG94Zm9EWkNvZmw0NjI2N3p1Q3ZSRUFTRjdpSEFDWDRnSmIzIiwiYXVkaWVuY2UiOiJodHRwczovL2FwaS5kaWdpdGFsb3dsLmFwcCIsImdyYW50X3R5cGUiOiJjbGllbnRfY3JlZGVudGlhbHMifQ==";
         private static Dictionary<string, string> bLines = new Dictionary<string, string>
         {
@@ -52,14 +53,70 @@ namespace DigitalOwl_Download
                 var download = await DownloadFile(item.id, item.name);
                 if (download)
                 {
-                    WriteToExcel(item);
-                    await ArchiveCase(item.name, item.id);
+                    var row = WriteToExcel(item);
+                    if (row == -1)
+                    {
+                        throw new Exception("Failed to write to excel file");
+                    }
+                    var fok = await ArchiveCase(item.name, item.id);
+                    if (fok)
+                    {
+                        await UpdateExcelArchived(row);
+                    }
                 }
                 
             }
         }
 
-        private static void WriteToExcel(Completed item)
+        static Task UpdateExcelArchived(int row)
+        {
+            Excel._Worksheet xlWorksheet = null;
+            Excel.Workbook xlWorkbook = null;
+            Excel.Application xlApp = null;
+            try
+            {
+                xlApp = new Excel.Application();
+                xlApp.Visible = false;
+                xlWorkbook = xlApp.Workbooks.Open(excelFile);
+                xlWorksheet = (Excel._Worksheet)xlWorkbook.ActiveSheet;
+                xlWorksheet.Range[E_STATUS + row, E_STATUS + row].Value2 = ArchiveText;
+                xlWorkbook.Save();
+                xlWorkbook.Close();
+                xlApp.Quit();
+            }
+            catch (Exception ex)
+            {
+                xlWorkbook.Close();
+                xlApp.Quit();
+                SimpleLogger.SimpleLog.Log(ex);
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                if (xlWorksheet != null)
+                {
+                    Marshal.ReleaseComObject(xlWorksheet);
+                }
+                //close and release
+                if (xlWorkbook != null)
+                {
+                    Marshal.ReleaseComObject(xlWorkbook);
+                }
+
+                if (xlApp != null)
+                {
+                    //quit and release
+
+                    Marshal.ReleaseComObject(xlApp);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static int WriteToExcel(Completed item)
         {
             Excel._Worksheet xlWorksheet = null;
             Excel.Workbook xlWorkbook = null;
@@ -72,15 +129,19 @@ namespace DigitalOwl_Download
                 xlWorksheet = (Excel._Worksheet)xlWorkbook.ActiveSheet;
                 var lastRow = xlWorksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing).Row;
                 bool found = false;
+                var actualRow = lastRow;
                 for (int i = lastRow; i > 1; i--)
                 {
                     var name = xlWorksheet.Range[E_NAME + i, E_NAME + i].Value2.ToString();
                     var status = xlWorksheet.Range[E_STATUS + i, E_STATUS + i].Value2.ToString();
-                    if (name == item.name && status == "העלה")
+                    if (name == item.name && status != ArchiveText)
                     {
                         found = true;
                         xlWorksheet.Range[E_DATEDOWNLOAD + i, E_DATEDOWNLOAD + i].Value2 = FormatExcelDate(DateTime.Now);
-                        xlWorksheet.Range[E_NUMPAGES + i, E_NUMPAGES + i].Value2 = item.pages.ToString();
+                        xlWorksheet.Range[E_NUMPAGES + i, E_NUMPAGES + i].Value2 = item.stats.pages.ToString();
+                        xlWorksheet.Range[E_MEDICALDATA + i, E_MEDICALDATA + i].Value2 = item.stats.mediaData.ToString();
+                        xlWorksheet.Range[E_HANDWRITTEN + i, E_HANDWRITTEN + i].Value2 = item.stats.handWritten.ToString();
+                        xlWorksheet.Range[E_OWLSTATUS + i, E_OWLSTATUS + i].Value2 = item.stats.status.ToString();
                         xlWorksheet.Range[E_STATUS + i, E_STATUS + i].Value2 = "הורדה";
 
                     }
@@ -88,10 +149,15 @@ namespace DigitalOwl_Download
                 if (!found)
                 {
                     var row = lastRow + 1;
+                    actualRow = row;
                     xlWorksheet.Range[E_DATE + row, E_DATE + row].Value2 = FormatExcelDate(DateTime.Now);
                     xlWorksheet.Range[E_NAME + row, E_NAME + row].Value2 = item.name;
                     xlWorksheet.Range[E_STATUS + row, E_STATUS + row].Value2 = "לא קיים באקסל";
-                    xlWorksheet.Range[E_NUMPAGES + row, E_NUMPAGES + row].Value2 = item.pages.ToString();
+                    xlWorksheet.Range[E_NUMPAGES + row, E_NUMPAGES + row].Value2 = item.stats.pages.ToString();
+                    xlWorksheet.Range[E_MEDICALDATA + row, E_MEDICALDATA + row].Value2 = item.stats.mediaData.ToString();
+                    xlWorksheet.Range[E_HANDWRITTEN + row, E_HANDWRITTEN + row].Value2 = item.stats.handWritten.ToString();
+                    xlWorksheet.Range[E_OWLSTATUS + row, E_OWLSTATUS + row].Value2 = item.stats.status.ToString();
+                    xlWorksheet.Range[E_BLINE + row, E_BLINE + row].Value2 = item.bline.ToString();
                     xlWorksheet.Range[E_DATEDOWNLOAD + row, E_DATEDOWNLOAD + row].Value2 = FormatExcelDate(DateTime.Now);
                 }
 
@@ -99,7 +165,7 @@ namespace DigitalOwl_Download
                 xlWorkbook.Save();
                 xlWorkbook.Close();
                 xlApp.Quit();
-                //
+                return actualRow;
 
             }
             catch (Exception ex)
@@ -130,6 +196,7 @@ namespace DigitalOwl_Download
                     Marshal.ReleaseComObject(xlApp);
                 }
             }
+            return -1;
         }
         static void ErrorToExcel(DirData data)
         {
@@ -232,46 +299,89 @@ namespace DigitalOwl_Download
         }
         private static async Task<List<Completed>> GetCasesAsync()
         {
+            var cases = new List<Completed>();
             try
             {
-                string bLineID;
-                if (!bLines.TryGetValue(CurrentBLine, out bLineID))
+                var lst = await GetAllBuisnessLines();
+                foreach (KeyValuePair<string, string> entry in lst)
                 {
-                    bLineID = await GetBLineID(CurrentBLine);
-                }
-                if (bLineID == null || bLineID == "ERROR")
-                {
-                    SimpleLogger.SimpleLog.Info("No BuisnessLine exist for provided value");
-                    throw new Exception("No BuisnessLine exist for provided value");
-                }
-                using (var client = new HttpClient())
-                {
-                    var request = new HttpRequestMessage()
+                    var bLineID = entry.Value;
+                    using (var client = new HttpClient())
                     {
-                        RequestUri = new Uri("https://api.digitalowl.app/cases"),
-                        Method = HttpMethod.Get
-
-                    };
-
-                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + KEY);
-
-                    using (var response = await client.SendAsync(request))
-                    {
-                        var data = await response.Content.ReadAsStringAsync();
-                        var root = (JArray)JsonConvert.DeserializeObject(data);
-                        if (root.Count == 0)
+                        var request = new HttpRequestMessage()
                         {
-                            return null;
+                            RequestUri = new Uri("https://api.digitalowl.app/cases"),
+                            Method = HttpMethod.Get
+
+                        };
+
+                        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + KEY);
+
+                        using (var response = await client.SendAsync(request))
+                        {
+                            var data = await response.Content.ReadAsStringAsync();
+                            var root = (JArray)JsonConvert.DeserializeObject(data);
+                            if (root.Count == 0)
+                            {
+                                return null;
+                            }
+                            var query = root
+                                .Where(r => (string)r["businessLineId"] == bLineID && (string)r["externalStatus"] == "completed")
+                                .Select(s => new Completed { id = (string)s["id"],  name = (string)s["name"], bline = entry.Key })
+                                .ToList();
+
+
+                            cases.AddRange(query);
                         }
-                        var query = root
-                            .Where(r => (string)r["businessLineId"] == bLineID && (string)r["externalStatus"] == "completed")
-                            .Select(s => new Completed { id = (string)s["id"], pages = (int)s["pageCount"], name = (string)s["name"] })
-                            .ToList();
-
-
-                        return query;
                     }
                 }
+                for (int i = 0; i < cases.Count(); i++)
+                {
+                    var completedCase = cases[i];
+                    using (var client = new HttpClient())
+                    {
+                        try
+                        {
+                            var request = new HttpRequestMessage()
+                            {
+                                RequestUri = new Uri("https://api.digitalowl.app/cases/" + completedCase.id + "/statistics"),
+                                Method = HttpMethod.Get
+
+                            };
+
+                            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + KEY);
+
+                            using (var response = await client.SendAsync(request))
+                            {
+                                response.EnsureSuccessStatusCode();
+                                var data = await response.Content.ReadAsStringAsync();
+                                var root = (JArray)JsonConvert.DeserializeObject(data);
+                                if (root.Count == 0)
+                                {
+                                    return null;
+                                }
+                                var query = root
+                                    .Select(s => new Stats { pages = (int)s["totalPageCount"], mediaData = (int)s["pagesWithMedicalDataCount"], handWritten = (int)s["handWrittenPageCount"], status = (string)s["status"] })
+                                    .FirstOrDefault();
+
+
+                                completedCase.stats = query;
+                            }
+                        }
+                        catch 
+                        {
+                            completedCase.stats = new Stats
+                            {
+                                pages = -1,
+                                mediaData = -1,
+                                handWritten = -1,
+                                status = ""
+                            };
+                        }
+                        
+                    }
+                }
+                return cases;
             }
                     
             catch (Exception ex)
@@ -282,7 +392,7 @@ namespace DigitalOwl_Download
                 return new List<Completed>();
             }
         }
-        private static async Task<string> GetBLineID(string bline)
+        private static async Task<string> GetBLineIdFromOwl(string bline)
         {
             try
             {
@@ -322,7 +432,7 @@ namespace DigitalOwl_Download
                 return "ERROR";
             }
         }
-        private static async Task ArchiveCase(string name, string caseId)
+        private static async Task<bool> ArchiveCase(string name, string caseId)
         {
             try
             {
@@ -341,6 +451,7 @@ namespace DigitalOwl_Download
                         response.EnsureSuccessStatusCode();
                     }
                 }
+                return true;
 
             }
             catch (Exception ex)
@@ -349,7 +460,70 @@ namespace DigitalOwl_Download
                 SimpleLogger.SimpleLog.Info("Error while archiving a case. Case ID - " + name);
                 SimpleLogger.SimpleLog.Log(ex);
                 BuildError(name, "Error while archiving a case. - " + ex.Message, "הורדה");
+                return false;
             }
+        }
+        private static async Task<Dictionary<string,string>> GetAllBuisnessLines()
+        {
+            var lst = new Dictionary<string, string>();
+            string bLineID;
+            if (!bLines.TryGetValue(CurrentBLine, out bLineID))
+            {
+                SimpleLogger.SimpleLog.Info("Default BuisnessLine is corrupt or not set in Config file");
+                throw new Exception("Default BuisnessLine is corrupt or not set in Config file");
+            }
+            lst.Add(CurrentBLine,bLineID);
+            Excel._Worksheet xlWorksheet = null;
+            Excel.Workbook xlWorkbook = null;
+            Excel.Application xlApp = null;
+            var excelFileName = Path.GetFileName(excelFile);
+            var bLineExcelFile = excelFile.Replace(excelFileName, "BusinessLine.csv");
+            try
+            {
+                xlApp = new Excel.Application();
+                xlApp.Visible = false;
+                xlWorkbook = xlApp.Workbooks.Open(bLineExcelFile);
+                xlWorksheet = (Excel._Worksheet)xlWorkbook.ActiveSheet;
+                var lastRow = xlWorksheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing).Row;
+                for (int i = 2; i <= lastRow; i++)
+                {
+                    var name = xlWorksheet.Range["A" + i, "A" + i].Value2.ToString();
+                    var bline = xlWorksheet.Range["B" + i, "B" + i].Value2.ToString();
+                    bLineID = await GetBLineIdFromOwl(bline);
+                    lst.Add(name, bLineID);
+                }
+                return lst;
+            }
+            catch (Exception ex)
+            {
+                SimpleLogger.SimpleLog.Log(ex);
+                xlWorkbook.Close();
+                xlApp.Quit();
+
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                if (xlWorksheet != null)
+                {
+                    Marshal.ReleaseComObject(xlWorksheet);
+                }
+                //close and release
+                if (xlWorkbook != null)
+                {
+                    Marshal.ReleaseComObject(xlWorkbook);
+                }
+
+                if (xlApp != null)
+                {
+                    //quit and release
+
+                    Marshal.ReleaseComObject(xlApp);
+                }
+            }
+            return null;
         }
         private static void BuildError(string name, string msg, string type = "העלה")
         {
@@ -366,21 +540,34 @@ namespace DigitalOwl_Download
         {
             return dt.ToString("dd/MM/yyyy H:mm:ss");
         }
+
         private static string E_DATE = "A";
         private static string E_NAME = "B";
         private static string E_NUMDOCS = "C";
         private static string E_NUMPAGES = "D";
-        private static string E_STATUS = "E";
-        private static string E_DATEUPLOAD = "F";
-        private static string E_DATEDOWNLOAD = "G";
-        private static string E_CONTINUE = "H";
-        private static string E_REMARK = "I";
+        private static string E_MEDICALDATA = "E";
+        private static string E_HANDWRITTEN = "F";
+        private static string E_STATUS = "G";
+        private static string E_OWLSTATUS = "H";
+        private static string E_DATEUPLOAD = "I";
+        private static string E_DATEDOWNLOAD = "J";
+        private static string E_CONTINUE = "K";
+        private static string E_BLINE = "L";
+        private static string E_REMARK = "M";
     }
     public  class Completed
     {
         public string id { get; set; }
-        public int pages { get; set; }
+        public Stats stats { get; set; }
         public string name { get; set; }
+        public string bline { get; set; }
+    }
+    public class Stats
+    {
+        public int pages { get; set; }
+        public int mediaData { get; set; }
+        public int handWritten { get; set; }
+        public string status { get; set; }
     }
     public class DirData
     {
